61de7796fcc8b956bc5c9e1d70da0f7c06e72b1f
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-01-26 21:50:36 +0300

    build homepage generation

---
+++ src/home.js
@@ -0,0 +1,49 @@
+const HomePage = (function() {
+    function generate() {
+        const phrase = document.createElement('p');
+        phrase.textContent = "Not only tasty, but pleasant...";
+        phrase.id = 'phrase';
+
+        const hugPic = document.createElement('img');
+        hugPic.src = "./assets/hug.png";
+        hugPic.alt = "two people are hugging";
+        hugPic.id = "hug-pic";
+
+        const question = document.createElement('h3');
+        question.id = "question";
+        question.textContent = "Have you ever been eating pizza alone with a strong desire to get hugs?";
+
+        const answer = document.createElement('h3');
+        answer.id = "answer";
+        answer.textContent = "We are going to solve your problem!";
+
+        const description = document.createElement('p');
+        description.id = "main-description";
+        description.textContent = "Here at PizzaHug you can eat pizza and hug everybody you want.\
+         Even if you don't eat pizza.";
+
+        
+        const homePageDiv = document.createElement('div');
+        homePageDiv.id = "home-page";
+        homePageDiv.classList.add("row-container");
+
+        const nodes = {
+            phrase,
+            hugPic,
+            question,
+            answer,
+            description,
+        };
+
+        for (const prop in nodes) {
+            homePageDiv.appendChild(nodes[prop]);
+        }
+
+        return homePageDiv;
+
+    }
+
+    return {generate};
+})();
+
+export default HomePage;
