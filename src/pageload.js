a2c80cd8a97decad0c0e8ca83ce3f73134034464
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-01-26 21:49:49 +0300

    build page generation

---
+++ src/pageload.js
@@ -0,0 +1,73 @@
+import HomePage from './home';
+
+function createHeader() {
+    const header = document.createElement('header');
+    const h1 = document.createElement('h1');
+
+    h1.textContent = 'PizzaHug';
+
+    header.appendChild(h1);
+
+    header.classList.add('row-container');
+
+    return header;
+}
+
+function createSidebar() {
+    const homeBtn = document.createElement('button');
+    const menuBtn = document.createElement('button');
+    const contactBtn = document.createElement('button');
+
+    homeBtn.id = "home-btn";
+    menuBtn.id = "menu-btn";
+    contactBtn.id = "contact-btn";
+
+    homeBtn.textContent = "Home";
+    menuBtn.textContent = "Menu";
+    contactBtn.textContent = "Contact us";
+
+    const buttons = [homeBtn, menuBtn, contactBtn];
+
+    const ul = document.createElement('ul');
+    ul.classList.add('col-container');
+
+    //append ul with three li each containing a button
+    for (const btn of buttons) {
+        const li = document.createElement('li');
+        ul.appendChild(li);
+
+        li.appendChild(btn);
+    }
+
+    const nav = document.createElement('nav');
+
+    nav.appendChild(ul);
+    nav.classList.add('col-container');
+
+    const aside = document.createElement('aside');
+    aside.classList.add('col-container');
+
+    aside.appendChild(nav);
+
+
+    const footer = document.createElement('footer');
+    footer.innerHTML = 'Made with love by <a href="https://github.com/twentysixhugs">twentysixhugs</a> :3';
+
+    aside.appendChild(footer);
+    
+    return aside;
+}
+
+function init() {
+    const header = createHeader();
+    const sidebar = createSidebar();
+    const homepage = HomePage.generate();
+
+    const content = document.querySelector('#content');
+
+    content.appendChild(header);
+    content.appendChild(sidebar);
+    content.appendChild(homepage);
+}
+
+export default init;
