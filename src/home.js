930da02be15c19c6caa100ebe571495091412851
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-01-27 20:19:50 +0300

    remove commented code blocks and refactor loop

--- src/home.js
+++ src/home.js
@@ -1,17 +1,9 @@
 const Homepage = (function() {
     function generate() {
-        // const hugPic = document.createElement('img');
-        // hugPic.src = "./assets/hug.png";
-        // hugPic.alt = "two people are hugging";
-        // hugPic.id = "hug-pic";
-
         const question = document.createElement('p');
         question.id = "question";
         question.textContent = "Have you ever been eating pizza alone with a strong desire to get hugs right now?";
 
-        // const answer = document.createElement('p');
-        // answer.id = "answer";
-        // answer.textContent = "We are going to solve your problem!";
 
         const description = document.createElement('p');
         description.id = "main-description";
@@ -23,16 +15,8 @@ const Homepage = (function() {
         homePageDiv.id = "home-page";
         homePageDiv.classList.add("col-container");
 
-        const nodes = {
-            question,
-            // answer,
-            // hugPic,
-            description,
-        };
-
-        for (const prop in nodes) {
-            homePageDiv.appendChild(nodes[prop]);
-        }
+        homePageDiv.appendChild(question);
+        homePageDiv.appendChild(description);
 
         return homePageDiv;
 
