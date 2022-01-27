b648d6826f7c2d4871f98cc128b0b4e8b02d3d70
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-01-27 21:26:52 +0300

    remove debugging tools

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,6 @@ const path = require('path');
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
-  devtool: 'inline-source-map',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
