6639eafae24c13d658957990689f2230ff295121
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-01-27 17:22:26 +0300

    make navbar buttons load pages

--- src/index.js
+++ src/index.js
@@ -1,7 +1,56 @@
 import init from './pageload';
+import Homepage from './home';
+import Menu from './menu';
+import Contacts from './contacts';
 
 init();
 
+window.onload = function() {
+    const buttons = Array.from(document.querySelectorAll("nav button"));
+    buttons.forEach(btn => btn.addEventListener('click', btnClickHandler));
+    
+    const deselectAll = function() {
+        for (const btn of buttons) {
+            btn.classList.remove('selected');
+        }
+    }
+
+    function btnClickHandler(e) {
+        if (!e.target.classList.contains("selected")) { // if the button is not already selected
+            deselectAll();
+
+            e.target.classList.add("selected"); // select clicked button
+
+            switch(e.target.id) {
+                case "home-btn":
+                    renderPage(Homepage.generate());
+                    break;
+                case "menu-btn":
+                    renderPage(Menu.generate());
+                    break;
+                case "contacts-btn":
+                    renderPage(Contacts.generate());
+                    break;
+            }
+        }
+    }
+};
+
+function renderPage(newPage) {
+    const aside = document.querySelector("aside");
+    const previousPage = document.querySelector(".current-page");
+    previousPage.remove();
+
+    newPage.classList.add('current-page');
+
+    aside.insertAdjacentElement("beforebegin", newPage);
+}
+
+
+
+
+//должен убрать first-load с #home-page
 //очистка туть
 //один хендлер для кнопки, в нем внутри функция, куда передается что надо
 //должен быть иф, который проверит, что страница уже не сгенерирована.
+//добавить класс selected для buttons
