3ce63070056f4fe7ddf51c41dede4fea61f8deb4
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-01-27 11:17:49 +0300

    comment out answer

--- src/home.js
+++ src/home.js
@@ -29,9 +29,9 @@ const HomePage = (function() {
 
         const nodes = {
             phrase,
-            // hugPic,
             question,
             // answer,
+            // hugPic,
             description,
         };
 
